616e0ed89f45de0868baa09f577098ae699f650d
psayeed1990 <sayeedmondal1412@gmail.com>
2021-01-22 14:29:42 +0600

    added background to register page

--- client/src/pages/user/auth/register/index.js
+++ client/src/pages/user/auth/register/index.js
@@ -13,24 +13,46 @@ const Register = ()=>{
                 <div className="content" id={styles.registerContent}>
                     <div className="image-container">
                         {/* <Image src="/images/icons/balloons.svg" width="80" height="auto" /> */}
-                        <Image src="/images/icons/people-formal.svg" width="100" height="auto" />
+                        {/* <Image src="/images/icons/people.svg" width="300" height="auto" />  */}
                     </div>
                     <form>
                         <div className="form-group">
                             
-                            <input id="name" name="name" type="text" placeholder="Name" />
+                            <input id="name" autoFocus={true} name="name" type="text" placeholder="Name" autoComplete="new-password" />
                             <label for="name">Name</label>
+                        </div>
+                        
+                        <div className="form-group">
+                            
+                            <input id="email" name="email" type="email" placeholder="Email" autoComplete="new-password"
+/>
+                            <label for="email">Email</label>
                         </div>
                         <div className="form-group">
                             
-                            <input id="email" name="email" type="email" placeholder="Email" />
-                            <label for="email">Email</label>
+                            <input id="password" name="password" type="password" placeholder="Password" autoComplete="new-password" />
+                            <label for="password">Password</label>
+                        </div>
+                        <div className="form-group">
+                            
+                            <input id="password-again" name="passwordAgain" type="password" placeholder="Password again" autoComplete="new-password" />
+                            <label for="password-again">Password Again</label>
+                        </div>
+                       
+                            
+                            <input id="hidden" name="hidden" type="hidden" placeholder="hidden" autoComplete="on" />
+                            
+                        
+                        <div className="form-group">
+                            
+                            <input id="submit" type="submit" value="Signup" />
+                            
                         </div>
 
                     </form>
                     <div className="image-container">
-                        {/* <Image src="/images/icons/balloons.svg" width="80" height="auto" /> */}
-                        <Image src="/images/icons/buddha.svg" width="100" height="auto" />
+                        {/* <Image src="/images/icons/calender.svg" width="80" height="auto" /> */}
+                        {/* <Image src="/images/icons/buddha.svg" width="100" height="auto" /> */}
                     </div>
 
                 </div>
