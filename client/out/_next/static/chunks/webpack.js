ce6c646d098fefad4e8703a9664a39a67abf70c1
psayeed1990 <sayeedmondal1412@gmail.com>
2021-02-04 16:33:24 +0600

    added form for admin user

--- client/out/_next/static/chunks/webpack.js
+++ client/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6fcb4fb107816b3eddae";
+/******/ 	var hotCurrentHash = "bf5b6b96eb34f5977b0c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
