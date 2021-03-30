20fa47ca73c171de42587b01ce84ac90ab16384d
psayeed1990 <sayeedmondal1412@gmail.com>
2021-03-30 11:26:29 +0600

    added tooltip

--- client/src/hooks/iframeFunction.js
+++ client/src/hooks/iframeFunction.js
@@ -1,10 +1,35 @@
 const iframeFunction = () => {
   if (document.getElementById('edit-frame')) {
+    //grab iframe document
     var iframe = document.getElementById('edit-frame');
     var innerDoc = iframe.contentDocument
       ? iframe.contentDocument
       : iframe.contentWindow.document;
+    //if iframe oaded
     if (innerDoc.readyState == 'complete') {
+      //create tooltip
+      const tooltip = document.createElement('p');
+      tooltip.style.cssText =
+        'top: 0;left: 0;opacity:0;position: absolute;z-index: 1000;width:auto;padding:4px;color: #444;background: white;';
+
+      //set id for tooltip
+      const text = document.createTextNode('Click to edit!');
+      tooltip.setAttribute('id', 'tooltip-inside-editor');
+      tooltip.appendChild(text);
+      const iframeBody = innerDoc.getElementsByTagName('body')[0];
+      iframeBody.appendChild(tooltip);
+
+      const tooltipId = innerDoc.getElementById('tooltip-inside-editor');
+      //tooltip follow mouse function
+      function fn(e) {
+        tooltipId.style.opacity = 1;
+        tooltipId.style.left = e.pageX + 'px';
+        tooltipId.style.top = e.pageY + 'px';
+      }
+
+      innerDoc.addEventListener('mousemove', fn, false);
+
+      //list all the tag to edit
       const elementArrayList = [
         'p',
         'button',
@@ -24,13 +49,23 @@ const iframeFunction = () => {
             var path = innerDoc.location.pathname;
             var page = path.split('/').pop();
             console.log(page);
-            elementArray[
-              i
-            ].innerHTML = `Clicked &lt;${element}&gt; #-${i} of ${page}`;
+
+            //set other editable to non editable/normal
+            const editables = innerDoc.querySelectorAll('[contenteditable]');
+            for (var j = 0; j < editables.length; j++) {
+              editables[j].setAttribute('contenteditable', 'false');
+            }
+
+            //change innerhtml of tooltip
+            tooltipId.innerHTML = `Clicked &lt;${element}&gt; #-${i} of ${page}`;
+
+            //make content editable
+            elementArray[i].setAttribute('contenteditable', 'true');
           });
         }
       };
 
+      // run clicked function for each tag
       elementArrayList.forEach((element) => {
         const elementArray = innerDoc.getElementsByTagName(element);
         clicked(elementArray, element);
