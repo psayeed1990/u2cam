8938eab4effd1ebffc66ca51053a8a3627c39307
psayeed1990 <sayeedmondal1412@gmail.com>
2021-03-30 10:11:00 +0600

    tracked click

--- client/src/hooks/iframeFunction.js
+++ client/src/hooks/iframeFunction.js
@@ -6,31 +6,34 @@ const iframeFunction = () => {
       : iframe.contentWindow.document;
     if (innerDoc.readyState == 'complete') {
       const elementArrayList = [
-        { element: innerDoc.getElementsByTagName('p') },
-        { element: innerDoc.getElementsByTagName('button') },
-        { element: innerDoc.getElementsByTagName('h1') },
-        { element: innerDoc.getElementsByTagName('h2') },
-        { element: innerDoc.getElementsByTagName('h3') },
-        { element: innerDoc.getElementsByTagName('h4') },
-        { element: innerDoc.getElementsByTagName('h5') },
-        { element: innerDoc.getElementsByTagName('h6') },
-        { element: innerDoc.getElementsByTagName('a') },
+        'p',
+        'button',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'a',
       ];
 
-      const clicked = (elementArray) => {
+      const clicked = (elementArray, element) => {
         for (let i = 0; i < elementArray.length; i++) {
           elementArray[i].addEventListener('click', () => {
             //get file path
             var path = innerDoc.location.pathname;
             var page = path.split('/').pop();
             console.log(page);
-            elementArray[i].innerHTML = `Clicked ${page}`;
+            elementArray[
+              i
+            ].innerHTML = `Clicked <${element}> #-${i} of ${page}`;
           });
         }
       };
 
-      elementArrayList.forEach((el) => {
-        clicked(el.element);
+      elementArrayList.forEach((element) => {
+        const elementArray = innerDoc.getElementsByTagName(element);
+        clicked(elementArray, element);
       });
     }
   }
