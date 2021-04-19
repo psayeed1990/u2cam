141467a9a6f677ee105845e8b1f2ee7a6074f3d8
psayeed1990 <sayeedmondal1412@gmail.com>
2021-04-19 10:48:11 +0600

    added right click edit menu

--- client/out/_next/static/chunks/webpack.js
+++ client/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9a2f231aeba05c6b4a51";
+/******/ 	var hotCurrentHash = "d641f4ab02c9d206f714";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
