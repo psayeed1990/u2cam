80b1d4438cef140a546b5df5f7b09b4a6618a74c
psayeed1990 <sayeedmondal1412@gmail.com>
2021-04-29 18:27:08 +0600

    design done

--- client/src/components/popups/designPopup/index.js
+++ client/src/components/popups/designPopup/index.js
@@ -171,6 +171,10 @@ const DesignPopup = ({ showDesignPopupSet, showDesignPopup, setRightMenu }) => {
       /***********************************************************
        ***start font families how***
        ***********************************************************/
+      const fontFamily = getComputedStyle(selectedElement).fontFamily;
+      //font size
+      // const fontSizeValue = getComputedStyle(selectedElement).fontSize;
+      // fontSize(fontSizeValue);
 
       /***********************************************************
        ***end font families show***
@@ -205,6 +209,52 @@ const DesignPopup = ({ showDesignPopupSet, showDesignPopup, setRightMenu }) => {
       ).value;
     }
 
+    //font family
+    const fontFamilyValue = document.getElementById('wp364-font-family');
+    if (fontFamilyValue.value === 'select') {
+      // do nothing
+    } else {
+      selectedElement.style.fontFamily = fontFamilyValue.value;
+    }
+
+    //font-size:
+    const fontSize = document.getElementById('wp364-font-size');
+    if (fontSize.value > 0) {
+      selectedElement.style.fontSize = fontSize.value;
+    }
+
+    //font weight
+    const fontWeightValue = document.getElementById('wp364-font-weight');
+    if (fontWeightValue.value !== 'select') {
+      selectedElement.style.fontWeight = fontWeightValue.value;
+    }
+
+    //border width
+    const borderWidth = document.getElementById('wp364-border-width');
+    if (borderWidth.value > 0) {
+      selectedElement.style.borderWidth = borderWidth.value;
+    }
+
+    //list style
+
+    const listStyleValue = document.getElementById('wp364-list-style');
+    if (listStyleValue.value !== 'select') {
+      selectedElement.style.listStyle = listStyleValue.value;
+    }
+
+    const textDecorationValue = document.getElementById(
+      'wp364-text-decoration'
+    );
+    if (textDecorationValue.value !== 'select') {
+      selectedElement.style.textDecoration = textDecorationValue.value;
+    }
+
+    //border style
+    const borderStyle = document.getElementById('wp364-border-style');
+    if (borderStyle.value !== 'select') {
+      selectedElement.style.borderStyle = borderStyle.value;
+    }
+
     noChangeSet({ bordercolor: false, txtcolor: false, bordercolor: false });
     showDesignPopupSet(false);
     setRightMenu(false);
@@ -232,32 +282,37 @@ const DesignPopup = ({ showDesignPopupSet, showDesignPopup, setRightMenu }) => {
       </p>
       <p>
         Font Family:
-        <select>
+        <select id="wp364-font-family">
+          <option value="select" selected disabled>
+            Select
+          </option>
           {fonts.map((font) => {
-            return <option>{font}</option>;
+            return <option value={font}>{font}</option>;
           })}
         </select>
       </p>
       <p>
-        Font Size: <input type="number" />
+        Font Size:
+        <input type="number" id="wp364-font-size" defaultValue={0} />
         px
       </p>
       <p>
         Font Weight:
-        <select>
-          <option>thin</option>
-          <option>normal</option>
-          <option>bold</option>
-          <option>bolder</option>
-          <option>100</option>
-          <option>200</option>
-          <option>300</option>
-          <option>400</option>
-          <option>500</option>
-          <option>600</option>
-          <option>700</option>
-          <option>800</option>
-          <option>900</option>
+        <select id="wp364-font-weight">
+          <option value="select" selected disabled></option>
+          <option value="thin">thin</option>
+          <option value="normal">normal</option>
+          <option value="bold">bold</option>
+          <option value="bolder">bolder</option>
+          <option value="100">100</option>
+          <option value="200">200</option>
+          <option value="300">300</option>
+          <option value="400">400</option>
+          <option value="500">500</option>
+          <option value="600">600</option>
+          <option value="700">700</option>
+          <option value="800">800</option>
+          <option value="900">900</option>
         </select>
       </p>
       <p>
@@ -269,29 +324,46 @@ const DesignPopup = ({ showDesignPopupSet, showDesignPopup, setRightMenu }) => {
         />
       </p>
       <p>
+        border width:
+        <input type="number" id="wp364-border-width" defaultValue={0} />
+        px
+      </p>
+      <p>
+        Border Style:
+        <select id="wp364-border-style">
+          <option value="select" selected disabled></option>
+          <option value="none">none</option>
+          <option value="dotted">dotted</option>
+          <option value="dashed">Dashed</option>
+          <option value="solid">Solid</option>
+        </select>
+      </p>
+      <p>
         Text Decoration:
-        <select>
-          <option>none</option>
-          <option>overline</option>
-          <option>line-through</option>
-          <option>underline</option>
-          <option>overline underline</option>
-          <option>initial</option>
-          <option>inherit</option>
+        <select id="wp364-text-decoration">
+          <option value="select" selected disabled></option>
+          <option value="none">none</option>
+          <option value="overline">overline</option>
+          <option value="line-through">line-through</option>
+          <option value="underline">underline</option>
+          <option value="overline underline">overline underline</option>
+          <option value="initial">initial</option>
+          <option value="inherit">inherit</option>
         </select>
       </p>
       <p>
         List Style:
-        <select>
-          <option>circle</option>
-          <option>disc</option>
-          <option>square</option>
-          <option>none</option>
-          <option>inherit</option>
-          <option>decimal</option>
-          <option>decimal-leading-zero</option>
-          <option>upper-roman</option>
-          <option>lower-roman</option>
+        <select id="wp364-list-style">
+          <option value="select" selected disabled></option>
+          <option value="circle">circle</option>
+          <option value="disc">disc</option>
+          <option value="square">square</option>
+          <option value="none">none</option>
+          <option value="inherit">inherit</option>
+          <option value="decimal">decimal</option>
+          <option value="decimal-leading-zero">decimal-leading-zero</option>
+          <option value="upper-roman">upper-roman</option>
+          <option value="lower-roman">lower-roman</option>
         </select>
       </p>
       <button onClick={saveDesign}>Save</button>
